bace5604d9843a0b51981397347326d227587991
Mehmet Akif Tütüncü <m.akif.tutuncu@gmail.com>
2014-04-26 15:04:59 +0300

    Added some data to evolutions.

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -3,5 +3,23 @@
 INSERT INTO accounts(id, email, password)
 VALUES (1, "admin@peoplemeter.com", "ba3253876aed6bc22d4a6ff53d8406c6ad864195ed144ab5c87621b6c233b548baeae6956df346ec8c17f5ea10f35ee3cbc514797ed7ddd3145464e2a0bab413");
 
+INSERT INTO channels (id, name) VALUES
+(1, 'Trt'),
+(2, 'Atv'),
+(3, 'Show Tv'),
+(4, 'Kanal D'),
+(5, 'Fox');
+
+INSERT INTO houses (id, family_name, district, street, building_name, door_number, postal_code, town, city) VALUES
+(1, 'Hacihalil', 'Sogukkuyu Mah.', 'Girne Bulvari', 'Onur Apt.', '12', '35000', 'Bayrakli', 'Izmir'),
+(2, 'Tutuncu', 'Karapinar Mah.', 'B149 Sok.', 'Tutuncu Apt.', '12', '16300', 'Yildirim', 'Bursa');
+
+INSERT INTO people (id, name, birth_date, is_male, house_id, button_number) VALUES
+(1, 'Ezgi Hacihalil', '1992-03-13', 0, 1, 1),
+(2, 'Mehmet Akif Tutuncu', '1991-03-23', 1, 2, 1);
+
 # --- !Downs
 TRUNCATE accounts;
+TRUNCATE channels;
+TRUNCATE houses;
+TRUNCATE people;
