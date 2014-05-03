4b649bb0715a10a5391a14bf1412590b8fc4f201
Mehmet Akif Tütüncü <m.akif.tutuncu@gmail.com>
2014-05-03 13:40:01 +0300

    Added documentation to external record API.

--- app/controllers/Records.scala
+++ app/controllers/Records.scala
@@ -5,6 +5,15 @@ import models.{Record, House}
 import models.Record.RecordAsJSON
 
 object Records extends Controller {
+  /**
+   * Action to add a record
+   *
+   * POST requests will be sent from peoplemeter devices to this action as JSON
+   *
+   * An example body:
+   *
+   * {"deviceId":"7b970ec12c3d4b1e8eaf16c9c26c4539", "data":{"id":-1,"houseId":2,"buttonNumber":1,"channelId":3,"startTime":1399112900000,"endTime":1399112940000}}
+   */
   def addRecord = Action { implicit request =>
     request.body.asJson map { json =>
       (json \ "deviceId").asOpt[String] map { deviceId =>
